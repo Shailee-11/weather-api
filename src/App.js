07d99c27f586d4cae4cdac8611579a6b942931a0
Shailee-11 <shaileejaiswal07@gmail.com>
2023-11-06 12:17:18 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import SearchBar from './components/SearchBar';
+import Overlay from './components/OverLay';
 
-function App() {
+const App = () => {
+  const [searchResults, setSearchResults] = useState(null);
+  const [showOverlay, setShowOverlay] = useState(false);
+
+  const handleSearch = async (city) => {
+    const apiKey = '840de593b7028de6e424162454790fe5';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=[CITY_NAME]&units=metric&appid=[apiKey]`;
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setSearchResults(data);
+      setShowOverlay(true);
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+    }
+  };
+
+  const handleCloseOverlay = () => {
+    setShowOverlay(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <SearchBar onSearch={handleSearch} />
+      {showOverlay && (
+        <Overlay data={searchResults} onClose={handleCloseOverlay} />
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/OverLay/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const OverLay = ({ name, temp, main, description, onClose }) => {
+  return (
+    <div className="overlay">
+      <div className="overlay-content">
+        <h2>{name}</h2>
+        <p>Temperature: {temp}°C</p>
+        <p>Main: {main}</p>
+        <p>Description: {description}</p>
+        <button onClick={onClose}>Close</button>
+      </div>
+    </div>
+  );
+};
+
+export default OverLay;
